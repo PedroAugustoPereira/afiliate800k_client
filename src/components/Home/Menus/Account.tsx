e893fc5c590c78c5452287dc3099a9f615b7f465
PedroAugustoPereira <pereiraaugustopedro2021@gmail.com>
2024-03-01 02:01:14 -0300

    testando mudar url

--- src/components/Home/Menus/Account.tsx
+++ src/components/Home/Menus/Account.tsx
@@ -18,7 +18,7 @@ const Account = () => {
                <img
                   src={
                      user?.imagePerfil
-                        ? `afiliate800k-api.onrender.com/api/images/${user?.imagePerfil}`
+                        ? `https//afiliate800k-api.onrender.com/api/images/${user?.imagePerfil}`
                         : perfil
                   }
                   className="w-14 h-14 rounded-full border-2 border-primary"
