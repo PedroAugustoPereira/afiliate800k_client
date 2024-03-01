f962d10a4e350b86e691480246de8b9bd022470a
PedroAugustoPereira <pereiraaugustopedro2021@gmail.com>
2024-02-29 22:24:34 -0300

    mudando parte do login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,10 +2,20 @@ import '@/styles/Login.scss';
 import '@/styles/globals.scss';
 import 'react-toastify/dist/ReactToastify.css';
 
-import { useEffect, useState } from 'react';
+import {
+  useEffect,
+  useState,
+} from 'react';
 
-import { Link, useNavigate } from 'react-router-dom';
-import { toast, ToastContainer, ToastOptions } from 'react-toastify';
+import {
+  Link,
+  useNavigate,
+} from 'react-router-dom';
+import {
+  toast,
+  ToastContainer,
+  ToastOptions,
+} from 'react-toastify';
 
 import login from '@/assets/login.png';
 import isLoggedIn from '@/middlewares/auth/isLoggedIn';
@@ -31,7 +41,8 @@ const Login = () => {
 
    //verificação se o usuádrio está logado:
    useEffect(() => {
-      isLoggedIn();
+      if(!isLoggedIn()) navigate('/login');
+      
    }, []);
 
    //seta o valor dos estados conforme os inputs
